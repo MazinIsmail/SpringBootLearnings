efec76d8e3d160365940be86c29701df1dc02b74
bsakthiv <bharatha.sakthivel.ext@nokia.com>
2019-08-21 19:40:02 +0530

    commit AccountServiceTest

--- SpringBootLearnings/src/test/java/com/wipro/bank/service/AccountServiceTest.java
+++ SpringBootLearnings/src/test/java/com/wipro/bank/service/AccountServiceTest.java
@@ -83,9 +83,10 @@ public class AccountServiceTest {
         accountAll.setBalance(500.0);
         accountList.add(accountAll);
         when(mockAccountRepository.saveAll(accountList)).thenReturn(accountList);
+	}
         		
         		/*
-		 * Copyright 2019 the original author or authors.
+		 * Copyright 2019 the original author or authors.+
 		 *
 		 * Licensed under the Apache License, Version 2.0 (the "License");
 		 * you may not use this file except in compliance with the License.
@@ -99,8 +100,7 @@ public class AccountServiceTest {
 		 * See the License for the specific language governing permissions and
 		 * limitations under the License.
 		 */
-))
-	}
+
 
 	/*
 	 * private List<Account> asList(Account accountAllList) { // TODO Auto-generated
