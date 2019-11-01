f1acc10420bc2f77bf453e89e65a663989c3ed56
Joxin <jox.johny@gmail.com>
2019-11-01 11:21:05 +0530

    Joxin: chnages

--- SpringBootLearnings/src/main/java/com/springboot/bank/main/SpringLearningsApplication.java
+++ SpringBootLearnings/src/main/java/com/springboot/bank/main/SpringLearningsApplication.java
@@ -7,9 +7,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("com.wipro.bank*")
-@EntityScan("com.wipro.bank.entity*")
-@EnableJpaRepositories("com.wipro.bank.repository*")
+@ComponentScan("com.springboot.bank.*")
+@EntityScan("com.springboot.bank.entity*")
+@EnableJpaRepositories("com.springboot.bank.repository*")
 public class SpringLearningsApplication {
 
 	public static void main(String[] args) {
